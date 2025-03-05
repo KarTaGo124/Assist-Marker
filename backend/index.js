b85e1e628a15e6626dada967e9f1264c9f437863
KarTaGo124 <joseguillermogalvezpacori@gmail.com>
2025-03-05 12:45:56 -0500

    Backend and Frontend fourth version, going deployment

--- backend/index.js
+++ backend/index.js
@@ -8,9 +8,9 @@ const cors = require('cors');
 const fetch = require('node-fetch');
 
 AWS.config.update({
-  accessKeyId: process.env.AWS_ACCESS_KEY_ID,
-  secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
-  sessionToken: process.env.AWS_SESSION_TOKEN,
+  accessKeyId: process.env.aws_access_key_id,
+  secretAccessKey: process.env.aws_secret_access_key,
+  sessionToken: process.env.aws_session_token,
   region: process.env.AWS_REGION
 });
 
