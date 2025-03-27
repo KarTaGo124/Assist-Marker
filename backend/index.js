f4d8727a358e4d88ca5e7ec10bc458ec45ec8f20
KarTaGo124 <joseguillermogalvezpacori@gmail.com>
2025-03-27 14:59:54 -0500

    CORS

--- backend/index.js
+++ backend/index.js
@@ -17,7 +17,7 @@ const dynamo = new AWS.DynamoDB.DocumentClient();
 const app = express();
 
 const corsOptions = {
-  origin: [process.env.FRONTEND_URL],
+  origin: [process.env.FRONTEND_URL, "http://localhost:5173"],
   credentials: true
 };
 app.use(cors(corsOptions));
